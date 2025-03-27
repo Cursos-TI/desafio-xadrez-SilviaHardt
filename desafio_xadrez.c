8fc06c62b8123a14bddbb551092fe354c3b8aebb
Silvia Hardt <silvinha126@gmail.com>
2025-03-27 12:16:02 -0300

    desafio aventureiro

--- desafio_xadrez.c
+++ desafio_xadrez.c
@@ -26,23 +26,41 @@ int main() {
     int torre = 0; // move 5 casas para a direita
     int bispo = 0; // move 5 casas na diagonal para cima direita
     int rainha = 0; // move 8 casas para a esquerda
+    int cavalo = 1; // move 2 casas para baixo e 1 casa para esquerda
 
     //while para torre mover 5 casas para a direita
     while(torre < 5){
         printf("Direita\n");
         torre++;
     }
+    printf("\n");
 
     //do while para bispo mover 5 casas na diagonal para cima direita
     do{
         printf("Cima Direita\n");
         bispo++;
     }while(bispo < 5);
+    printf("\n");
 
     //for para rainha mover 8 casas para esquerda
     for(rainha = 0; rainha < 8; rainha++) {
         printf("Esquerda\n");
     }
+    printf("\n");
+
+    //loop aninhado utilizando for e while para cavalo mover 2 casas para baixo e 1 casa para esquerda
+    for (cavalo = 1; cavalo <= 1; cavalo++) {
+        
+        int movimento = 1;
+        while (movimento <= 2) {
+            
+            printf("Baixo\n");
+            movimento++;
+        }
+
+        printf("Esquerda\n");
+    }
+    printf("\n");
 
     return 0;
 }
